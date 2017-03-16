ac7fb5d473e1e6163fb32688f9c88c52fb55ab94
Jimmy Hong <jimmyhong.rocks@gmail.com>
2017-03-15 21:38:40 -0500

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,26 +2,26 @@
 var mysql = require("mysql");
 var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "y06qcehxdtkegbeb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  user: "i36rqeq3f7daim5k",
-  password: "p9pzsgwkpyn2m0g1",
-  database: "w9olf47ly4eztejd"
-});
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "y06qcehxdtkegbeb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//   user: "i36rqeq3f7daim5k",
+//   password: "p9pzsgwkpyn2m0g1",
+//   database: "w9olf47ly4eztejd"
+// });
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL)
-// } else {
-//   connection = mysql.createConnection({
-//     port: 3306,
-//     host: "localhost",
-//     user: "root",
-//     password: "",
-//     database: "burgers_db"
-//   });
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
 
-// }
+}
 
 // Make connection.
 connection.connect(function (err) {
